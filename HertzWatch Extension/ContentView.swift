b63c9085a8f0a6bd8247398155af84251ae17db0
Mikael Hultgren <blomma@artsoftheinsane.com>
2020-11-20 20:30:18 +0100

    Made it more clean

--- HertzWatch Extension/ContentView.swift
+++ HertzWatch Extension/ContentView.swift
@@ -9,135 +9,113 @@ struct ContentView: View {
     
     @ObservedObject var model = ContentViewModel(hertzModel: HertzModel())
     @ObservedObject var workoutManager: WorkoutManager = .shared
-
-    let dot = Color(red: 1, green: 0, blue: 0)
-
+    
     var body: some View {
-        ScrollView {
-            VStack {
-                GeometryReader { geometry in
-                    ZStack {
-                        VStack {
-                            ZStack {
-                                TickFace(model: model)
-                                    .frame(
-                                        width: geometry.size.width,
-                                        height: geometry.size.width
-                                    )
-                                    .mask(
-                                        Arc(
-                                            startAngle: .degrees(312),
-                                            endAngle: .degrees(47),
-                                            clockwise: true,
-                                            radiusOffset: 0
-                                        )
-                                        .fill(
-                                            LinearGradient(
-                                                gradient: Gradient(
-                                                    colors: [
-                                                        .clear,
-                                                        Color.gray.opacity(0.1),
-                                                        Color.gray.opacity(0.2),
-                                                        Color.gray.opacity(0.7),
-                                                        Color.gray.opacity(0.8),
-                                                        Color.gray.opacity(1),
-                                                        Color.gray.opacity(0.9),
-                                                        Color.gray.opacity(0.8),
-                                                        Color.gray.opacity(0.2),
-                                                        Color.gray.opacity(0.1),
-                                                        .clear,
-                                                    ]
-                                                ),
-                                                startPoint: .leading,
-                                                endPoint: .trailing
-                                            )
-                                        )
-                                        .rotationEffect(model.currentAngle)
-                                    )
-
-                                Dot(circleRadius: 6, fillColor: dot)
-                                    .rotationEffect(model.currentAngle)
-                                    .frame(
-                                        width: geometry.size.width - 38,
-                                        height: geometry.size.width - 12
-                                    )
-
-                                if !model.isRunning {
-                                    Button {
-                                        model.start()
-                                    } label: {
-                                        Image(systemName: "arrowtriangle.right.circle")
-                                    }
-                                    .buttonStyle(PlainButtonStyle())
-                                    .foregroundColor(.white)
-                                    .font(Font.system(size: 50, weight: .ultraLight, design: .default))
-                                    .transition(
-                                        AnyTransition.opacity.animation(.easeInOut(duration: 1.0))
-                                    )
-                                } else {
-                                    VStack {
-                                        Text("\(model.heartRate, specifier: "%.1f") ♥")
-                                            .font(
-                                                Font.system(
-                                                    size: 20,
-                                                    weight: .regular,
-                                                    design: .default
-                                                ).monospacedDigit()
-                                            )
-                                        Text("Scroll: \(model.digitalScrollAmount)")
-                                            .font(
-                                                Font.system(
-                                                    size: 18,
-                                                    weight: .regular,
-                                                    design: .default
-                                                ).monospacedDigit()
-                                            )
-                                            .focusable(true)
-                                            .digitalCrownRotation($model.digitalScrollAmount, from: -2, through: 2, by: 1, sensitivity: .low, isContinuous: false, isHapticFeedbackEnabled: true)
-                                        Text("\(model.factor, specifier: "%.1f")")
-                                            .font(
-                                                Font.system(
-                                                    size: 18,
-                                                    weight: .regular,
-                                                    design: .default
-                                                ).monospacedDigit()
-                                            )
-         
-                                    }
-                                    .transition(
-                                        AnyTransition.opacity.animation(.easeInOut(duration: 1.0))
-                                    )
-                                }
-                            }
-                        }
-                        .background(
-                            Color(
-                                red: 0,
-                                green: 0,
-                                blue: 0
-                            )
-                            .edgesIgnoringSafeArea(.all)
+        ZStack {
+            Group {
+                TickFace(model: model)
+                    .mask(
+                        Arc(
+                            startAngle: .degrees(312),
+                            endAngle: .degrees(47),
+                            clockwise: true,
+                            radiusOffset: 0
                         )
-                        .disabled(showOnboarding)
-                        .blur(radius: showOnboarding ? 3.0 : 0)
-
-                        if showOnboarding {
-                            OnBoardingScreenView(isPresenting: $showOnboarding, model: onBoardingModel)
-                        }
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(
+                                    colors: [
+                                        .clear,
+                                        Color.gray.opacity(0.1),
+                                        Color.gray.opacity(0.2),
+                                        Color.gray.opacity(0.7),
+                                        Color.gray.opacity(0.8),
+                                        Color.gray.opacity(1),
+                                        Color.gray.opacity(0.9),
+                                        Color.gray.opacity(0.8),
+                                        Color.gray.opacity(0.2),
+                                        Color.gray.opacity(0.1),
+                                        .clear,
+                                    ]
+                                ),
+                                startPoint: .leading,
+                                endPoint: .trailing
+                            )
+                        )
+                        .rotationEffect(model.currentAngle)
+                    )
+                
+                Dot(circleRadius: 6, fillColor: Color(red: 1, green: 0, blue: 0))
+                    .rotationEffect(model.currentAngle)
+                    .padding(5)
+                
+                if !model.isRunning {
+                    Button {
+                        model.start()
+                    } label: {
+                        Image(systemName: "arrowtriangle.right.circle")
                     }
-                    .onAppear {
-                        hasOnboarded = false // DEBUG
-                        if !hasOnboarded {
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                withAnimation {
-                                    showOnboarding.toggle()
-                                    hasOnboarded = true
-                                }
-                            }
-                        }
+                    .buttonStyle(PlainButtonStyle())
+                    .foregroundColor(.white)
+                    .font(Font.system(size: 50, weight: .ultraLight, design: .default))
+                    .transition(
+                        AnyTransition.opacity.animation(.easeInOut(duration: 1.0))
+                    )
+                } else {
+                    VStack {
+                        Text("\(model.heartRate, specifier: "%.1f") ♥")
+                            .font(
+                                Font.system(
+                                    size: 20,
+                                    weight: .regular,
+                                    design: .default
+                                ).monospacedDigit()
+                            )
+                        Text("Scroll: \(model.digitalScrollAmount)")
+                            .font(
+                                Font.system(
+                                    size: 18,
+                                    weight: .regular,
+                                    design: .default
+                                ).monospacedDigit()
+                            )
+                        Text("\(model.factor, specifier: "%.1f")")
+                            .font(
+                                Font.system(
+                                    size: 18,
+                                    weight: .regular,
+                                    design: .default
+                                ).monospacedDigit()
+                            )
+                        
+                    }
+                    .transition(
+                        AnyTransition.opacity.animation(.easeInOut(duration: 1.0))
+                    )
+                }
+            }
+            .disabled(showOnboarding)
+            .blur(radius: showOnboarding ? 3.0 : 0)
+            
+            if showOnboarding {
+                OnBoardingScreenView(isPresenting: $showOnboarding, model: onBoardingModel)
+            }
+        }
+        .focusable()
+        .digitalCrownRotation($model.digitalScrollAmount, from: -2, through: 2, by: 1, sensitivity: .low, isContinuous: false, isHapticFeedbackEnabled: true)
+        .background(
+            Color(.black)
+            .edgesIgnoringSafeArea(.all)
+        )
+        .onAppear {
+            hasOnboarded = false // DEBUG
+            if !hasOnboarded {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    withAnimation {
+                        showOnboarding.toggle()
+                        hasOnboarded = true
                     }
                 }
-                //InstructionView()
             }
         }
     }
--- HertzWatch Extension/Faces/TickFace.swift
+++ HertzWatch Extension/Faces/TickFace.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct TickFace: View {
     var model: ContentViewModel
-
+    
     var body: some View {
         ZStack {
             ForEach(self.model.ticks, id: \.self) { tick in
@@ -12,12 +12,13 @@ struct TickFace: View {
                             self.model.getColor(for: tick.segment)
                         )
                         .frame(width: 7, height: 20)
-                        .shadow(color: .black, radius: 1, x: -0.5, y: -0.5)
                     Spacer()
                 }
                 .rotationEffect(tick.angle)
             }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .aspectRatio(1.0, contentMode: .fit)
     }
 }
 
