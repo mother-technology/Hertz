0ad7334abe02c6f4643fd29fddaf40e53ecef881
Mikael Hultgren <blomma@artsoftheinsane.com>
2020-11-28 09:55:52 +0100

    Save the heartbeat per minute during Hertzing to HK

--- HertzWatch Extension/WorkoutManager.swift
+++ HertzWatch Extension/WorkoutManager.swift
@@ -2,44 +2,124 @@ import Combine
 import Foundation
 import HealthKit
 
+//// DEBUG
+//let workoutPredicate = HKQuery.predicateForWorkouts(with: .mindAndBody)
+//let sampleType = HKObjectType.workoutType()
+//let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+//let limit = 0
+//
+//let query = HKSampleQuery(
+//    sampleType: sampleType,
+//    predicate: workoutPredicate,
+//    limit: limit,
+//    sortDescriptors: [sortDescriptor]) { (query, results, error) in
+//        // TODO: - Error handling, Mikael
+//        var workouts: [HKWorkout] = []
+//        if let results = results {
+//            for result in results {
+//                if let workout = result as? HKWorkout, workout.sourceRevision.source.name == "Hertz" {
+//                    workouts.append(workout)
+//                }
+//            }
+//        }
+//
+//        guard let latest = workouts.first else {
+//            return
+//        }
+//
+//        guard let distanceType =
+//                HKObjectType.quantityType(forIdentifier: .heartRate) else {
+//            fatalError("*** Unable to create a distance type ***")
+//        }
+//        let workoutPredicate = HKQuery.predicateForObjects(from: latest)
+//        let startDateSort = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: true)
+//
+//        let query = HKSampleQuery(sampleType: distanceType,
+//                                  predicate: workoutPredicate,
+//                                  limit: 0,
+//                                  sortDescriptors: [startDateSort]) { (sampleQuery, results, error) -> Void in
+//            print(error)
+//            guard let distanceSamples = results as? [HKQuantitySample] else {
+//                // Perform proper error handling here.
+//                return
+//            }
+//
+//            print(distanceSamples)
+//            // Use the workout's distance samples here.
+//        }
+//
+//        healthStore?.execute(query)
+//}
+//
+//healthStore?.execute(query)
+
 final class WorkoutManager: NSObject, ObservableObject {
     static let shared = WorkoutManager()
-
-    let healthStore = HKHealthStore()
-    var session: HKWorkoutSession?
-    var builder: HKLiveWorkoutBuilder!
-
-    let builderDelegate = WorkoutManagerBuilderDelegate()
+    
+    // Metadata
+    let HeartrateMetadataBaseLine = "baseline"
+    
+    private var healthStore: HKHealthStore?
+    private var session: HKWorkoutSession?
+    private var builder: HKLiveWorkoutBuilder?
+    
+    private let builderDelegate = WorkoutManagerBuilderDelegate()
     
     let publisher: AnyPublisher<Double, Never>
     let subject = PassthroughSubject<Double, Never>()
     
+    private var heartRateSamples: [HKQuantitySample] = []
+    private var startDate: Date?
+    
     override init() {
         publisher = subject.eraseToAnyPublisher()
+        
+        if !HKHealthStore.isHealthDataAvailable() {
+            return
+        }
+        
+        healthStore = HKHealthStore()
     }
-
+    
     func requestAuthorization() {
+        if !HKHealthStore.isHealthDataAvailable() {
+            return
+        }
+        
         let typesToShare: Set = [
-            HKQuantityType.workoutType(),
+            HKQuantityType.workoutType()
         ]
-
+        
         let typesToRead: Set = [
             HKQuantityType.quantityType(forIdentifier: .heartRate)!,
+            HKQuantityType.workoutType()
         ]
-
-        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { _, _ in
+        
+        healthStore?.requestAuthorization(toShare: typesToShare, read: typesToRead) { success, error in
+            // TODO: - Error handling, Mikael
         }
     }
-
-    func workoutConfiguration() -> HKWorkoutConfiguration {
+    
+    private func workoutConfiguration() -> HKWorkoutConfiguration {
         let configuration = HKWorkoutConfiguration()
         configuration.activityType = .mindAndBody
         configuration.locationType = .unknown
-
+        
         return configuration
     }
-
+    
     func startWorkout() {
+        if !HKHealthStore.isHealthDataAvailable() {
+            return
+        }
+        
+        guard let healthStore = healthStore else {
+            // TODO: - Error handling, Mikael
+            fatalError("*** Healthstore Not Available ***")
+        }
+        
+        heartRateSamples.removeAll()
+        
         do {
             session = try HKWorkoutSession(
                 healthStore: healthStore,
@@ -47,43 +127,80 @@ final class WorkoutManager: NSObject, ObservableObject {
             )
             builder = session?.associatedWorkoutBuilder()
         } catch {
+            // TODO: - Error handling, Mikael
             return
         }
-
+        
         builderDelegate.action = { statistics in
             self.updateForStatistics(statistics)
         }
-        builder.delegate = builderDelegate
-
+        builder?.delegate = builderDelegate
         session?.delegate = self
-
-        builder.dataSource = HKLiveWorkoutDataSource(
+        builder?.dataSource = HKLiveWorkoutDataSource(
             healthStore: healthStore,
             workoutConfiguration: workoutConfiguration()
         )
-
-        session?.startActivity(with: Date())
-        builder.beginCollection(withStart: Date()) { _, _ in
+        
+        startDate = Date()
+        session?.startActivity(with: startDate!)
+        builder?.beginCollection(withStart: startDate!) { success, error in
+            // TODO: - Error handling, Mikael
         }
     }
+    
+    func update(baseline withValue: Double) {
+        guard let heartRateQuantityType = HKSampleType.quantityType(forIdentifier: .heartRate) else {
+            // TODO: - Error handling, Mikael
+            fatalError("*** Heartrate Type Not Available ***")
+        }
 
+        let metadata: [String: Any] = [HeartrateMetadataBaseLine: 1]
+        let heartRateUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
+        let now = Date()
+        
+        let sample = HKQuantitySample(type: heartRateQuantityType, quantity: .init(unit: heartRateUnit, doubleValue: withValue), start: now, end: now, metadata: metadata)
+        
+        heartRateSamples.append(sample)
+    }
+    
     func endWorkout() {
-        if session?.state == .running {
-            session?.end()
-        }
-    }
-
-    func updateForStatistics(_ statistics: HKStatistics?) {
-        guard let statistics = statistics else {
+        if !HKHealthStore.isHealthDataAvailable() {
             return
         }
-
-        switch statistics.quantityType {
-        case HKQuantityType.quantityType(forIdentifier: .heartRate):
+        
+        if session?.state == .running {
+            session?.end()
+            
+            builder?.add(heartRateSamples, completion: { [self] (success, error) in
+                // TODO: - Error handling, Mikael
+                
+                builder?.endCollection(withEnd: Date()) { [self] success, error in
+                    // TODO: - Error handling, Mikael
+                    
+                    builder?.finishWorkout { [self] workout, error in
+                        // TODO: - Error handling, Mikael
+                    }
+                }
+            })
+        }
+    }
+    
+    private func updateForStatistics(_ statistics: HKStatistics?) {
+        guard let heartRateQuantityType = HKSampleType.quantityType(forIdentifier: .heartRate) else {
+            // TODO: - Error handling, Mikael
+            fatalError("*** Heartrate Type Not Available ***")
+        }
+        
+        switch statistics?.quantityType {
+        case heartRateQuantityType:
             let heartRateUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
-            let value = statistics.mostRecentQuantity()?.doubleValue(for: heartRateUnit)
-            let roundedValue = Double(round(1 * value!) / 1)
+            guard let value = statistics?.mostRecentQuantity()?.doubleValue(for: heartRateUnit) else { return }
+            let roundedValue = Double(round(1 * value) / 1)
 
+            let now = Date()
+            let sample = HKQuantitySample(type: heartRateQuantityType, quantity: .init(unit: heartRateUnit, doubleValue: roundedValue), start: now, end: now)
+            heartRateSamples.append(sample)
+            
             self.subject.send(roundedValue)
         default:
             return
@@ -91,17 +208,17 @@ final class WorkoutManager: NSObject, ObservableObject {
     }
 }
 
-class WorkoutManagerBuilderDelegate: NSObject, HKLiveWorkoutBuilderDelegate {
+private class WorkoutManagerBuilderDelegate: NSObject, HKLiveWorkoutBuilderDelegate {
     var action: ((_ statistics: HKStatistics?) -> Void)?
-
+    
     func workoutBuilderDidCollectEvent(_: HKLiveWorkoutBuilder) {}
-
+    
     func workoutBuilder(_ workoutBuilder: HKLiveWorkoutBuilder, didCollectDataOf collectedTypes: Set<HKSampleType>) {
         for type in collectedTypes {
             guard let quantityType = type as? HKQuantityType else {
                 return // Nothing to do.
             }
-
+            
             let statistics = workoutBuilder.statistics(for: quantityType)
             action?(statistics)
         }
@@ -110,15 +227,7 @@ class WorkoutManagerBuilderDelegate: NSObject, HKLiveWorkoutBuilderDelegate {
 
 extension WorkoutManager: HKWorkoutSessionDelegate {
     func workoutSession(_: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState,
-                        from _: HKWorkoutSessionState, date _: Date) {
-        if toState == .ended {
-            builder.endCollection(withEnd: Date()) { _, _ in
-                self.builder.finishWorkout { _, _ in
-                    // Optionally display a workout summary to the user.
-                }
-            }
-        }
-    }
-
+                        from _: HKWorkoutSessionState, date _: Date) {}
+    
     func workoutSession(_: HKWorkoutSession, didFailWithError _: Error) {}
 }
